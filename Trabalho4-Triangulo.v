94023f80d0f6194a90c9f68a5a36e02c37af824a
Daniel Vitor <37313548+laghins@users.noreply.github.com>
2018-07-09 00:43:38 -0300

    Update Trabalho4-Triangulo.v

--- Trabalho4-Triangulo.v
+++ Trabalho4-Triangulo.v
@@ -5,7 +5,7 @@ int areadotri(int x1, int y1, int x2, int y2, int x3, int y3){
    return abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
 }
 
-int isInside(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y){
+int dentrodaarea(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y){
    int A = areadotri (x1, y1, x2, y2, x3, y3);
    int A1 = areadotri (x, y, x2, y2, x3, y3);
    int A2 = areadotri (x1, y1, x, y, x3, y3);
@@ -24,7 +24,7 @@ int main(void){
    system("clear");
    for(int i = 1; i <= 48; i++){
       for(int j = 1; j <= 64; j++){
-         printf(isInside(y[0], x[0], y[1], x[1], y[2], x[2], i, j) ? "*" : " ");
+         printf(dentrodaarea(y[0], x[0], y[1], x[1], y[2], x[2], i, j) ? "*" : " ");
       }
       printf("\n");
    }
