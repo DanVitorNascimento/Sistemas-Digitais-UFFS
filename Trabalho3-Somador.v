34d042f1fe2a45ef56d4cf567fff018c65170369
Daniel Vitor <37313548+laghins@users.noreply.github.com>
2018-07-09 00:23:55 -0300

    Update Trabalho3-Somador.v

--- Trabalho3-Somador.v
+++ Trabalho3-Somador.v
@@ -1,40 +1,40 @@
-module somador(input clock,
-	input [43:0] E1,
-	input [43:0] E2,
+module somadors(input clock,
+		input [43:0] Entrada1,
+		input [43:0] Entrada2,
 	output[43:0] S0);
 
 	reg [43:0] saida = 0;
 	reg [43:0] aux = 0;
  	assign S0 = saida;
 	always @(posedge clock) begin
-		aux <= E1 + E2;
+		aux <= Entrada1 + Entrada2;
 		saida = aux + saida;
 	end
 endmodule
 
-module teste;
+module alpha;
 	reg clock;
-	reg[43:0] E1, E2;
+	reg[43:0] Entrada1, Entrada2;
 	wire[43:0] S0;
 	always #3 clock = ~clock;
-	somador B(clock, E1, E2, S0);
+	somadors B(clock, Entrada1, Entrada2, S0);
 	initial begin
 		$dumpvars();
 		#0 clock <= 0;
-		E1 <= 2;
-		E2 <= 2;
+		Entrada1 <= 2;
+		Entrada2 <= 2;
 		//4
 		#6
-		E1 <= 4;
-		E2 <= 4;
+		Entrada1 <= 4;
+		Entrada2 <= 4;
 		//12
 		#6
-		E1 <= 5;
-		E2 <= 5;
+		Entrada1 <= 5;
+		Entrada2 <= 5;
 		//22
 		#6
-		E1 <= 3;
-		E2 <= 3;
+		Entrada1 <= 3;
+		Entrada2 <= 3;
 		//28
 		#12
 		$finish;
